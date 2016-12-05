9609c164cabf7683acaf85c24661a3fb88eeea13
park <park.sung.ho@hotmail.com>
2016-12-05 18:36:15 +0900

    .

---
+++ check_test.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+while true;do 
+ echo "------------------------------------------------------------------"
+ date
+ free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%)\n", $3,$2,$3*100/$2 }' 
+ df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'  
+ top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' 
+ echo "------------------------------------------------------------------"; sleep 10; done;
---
+++ deleteFile.bat
@@ -0,0 +1,2 @@
+deleteFile.py c:\work\jp_b\out\artifacts\admin_war_exploded\WEB-INF\classes\log4j.xml
+deleteFile.py c:\work\jp_b\out\artifacts\front_war_exploded\WEB-INF\classes\log4j.xml
